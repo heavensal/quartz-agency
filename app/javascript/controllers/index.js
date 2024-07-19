8bfe6a9af2834e6a7139f0853f5ce9e68dee956a
adam lamouri <adamlamouri.pro@gmail.com>
2024-07-19 18:57:09 +0200

    Add libvips buildpack for Heroku

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,10 +4,12 @@ import { application } from "controllers/application"
 import ScrollReveal from '@stimulus-components/scroll-reveal'
 import Dialog from '@stimulus-components/dialog'
 import TextareaAutogrow from 'stimulus-textarea-autogrow'
+import ScrollProgress from '@stimulus-components/scroll-progress'
 
 application.register("scroll-reveal", ScrollReveal)
 application.register('dialog', Dialog)
 application.register('textarea-autogrow', TextareaAutogrow)
+application.register('scroll-progress', ScrollProgress)
 
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
