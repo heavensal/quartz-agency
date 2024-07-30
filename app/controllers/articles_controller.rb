4ff89f19b7b1f0711ceea262dfd61902322481da
adam lamouri <adamlamouri.pro@gmail.com>
2024-07-30 18:05:39 +0200

    n+1 queries countered

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
 
+
   def index
-    @articles = Article.all
+    @articles = Article.select(:id, :title, :summary, :published_at).order(published_at: :desc).with_attached_image
   end
 
   def show
